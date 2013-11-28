b4066c67579ff13291bced4d000cf3cd430cdb0e
Christof Pieloth <pieloth@labp.htwk-leipzig.de>
2013-11-28 18:19:18 +0100

    [FIX] Move factory() and getXPMIcon() to public as in WModule.

--- src/modules/mneRtClient/WMMneRtClient.h
+++ src/modules/mneRtClient/WMMneRtClient.h
@@ -70,6 +70,20 @@ public:
      */
     virtual const std::string getDescription() const;
 
+    /**
+     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
+     * should never be initialized or modified in some other way. A simple new instance is required.
+     *
+     * \return the prototype used to create every module in OpenWalnut.
+     */
+    virtual WModule::SPtr factory() const;
+
+    /**
+     * Get the icon for this module in XPM format.
+     * \return The icon.
+     */
+    virtual const char** getXPMIcon() const;
+
 protected:
     // ---------------------------------
     // Methods for WLEMMCommandProcessor
@@ -94,20 +108,6 @@ protected:
      * Initialize the properties for this module.
      */
     virtual void properties();
-
-    /**
-     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
-     * should never be initialized or modified in some other way. A simple new instance is required.
-     *
-     * \return the prototype used to create every module in OpenWalnut.
-     */
-    virtual WModule::SPtr factory() const;
-
-    /**
-     * Get the icon for this module in XPM format.
-     * \return The icon.
-     */
-    virtual const char** getXPMIcon() const;
 
 private:
     //! a condition for the matrix selection
