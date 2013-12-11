1a103d4b1f7d777c9d86f778a9515cb2a3b55808
Christof Pieloth <pieloth@labp.htwk-leipzig.de>
2013-12-11 17:20:27 +0100

    [ADD #256] Add module to read FIFF files and additional data. Should later replace EM Measurement.

---
+++ src/modules/fiffReader/WMFiffReader.cpp
@@ -0,0 +1,368 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <boost/filesystem.hpp>
+
+#include <core/common/WException.h>
+#include <core/common/WItemSelectionItemTyped.h>
+#include <core/common/WPathHelper.h>
+
+#include "core/data/emd/WLEMDEEG.h"
+#include "core/data/enum/WLEModality.h"
+#include "core/io/WLReaderFIFF.h"
+#include "core/util/WLGeometry.h"
+
+#include "WMFiffReader.h"
+#include "WMFiffReader.xpm"
+
+using std::string;
+using namespace LaBP;
+
+W_LOADABLE_MODULE( WMFiffReader )
+
+std::string WMFiffReader::EFileStatus::name( EFileStatus::Enum val )
+{
+    switch( val )
+    {
+        case NO_FILE:
+            return "No file";
+        case LOADING_FILE:
+            return "Loading file ...";
+        case FILE_ERROR:
+            return "Could not read file!";
+        case SUCCESS:
+            return "File successfully read!";
+        default:
+            return "Unknown status!";
+    }
+}
+
+std::string WMFiffReader::EDataStatus::name( EDataStatus::Enum val )
+{
+    switch( val )
+    {
+        case NO_DATA:
+            return "No additional data";
+        case DATA_AVAILABLE:
+            return "Additional data available";
+        case LOADING_DATA:
+            return "Loading data ...";
+        case DATA_ERROR:
+            return "Could not load data!";
+        case SUCCESS:
+            return "Data successfully read!";
+        default:
+            return "Unknown status!";
+    }
+}
+
+WMFiffReader::WMFiffReader()
+{
+    m_fileStatus = EFileStatus::NO_FILE;
+    m_dataStatus = EDataStatus::NO_DATA;
+}
+
+WMFiffReader::~WMFiffReader()
+{
+}
+
+const std::string WMFiffReader::getName() const
+{
+    return "FIFF Reader";
+}
+
+const std::string WMFiffReader::getDescription() const
+{
+    return "Reads a FIFF file and related/additional data if available.";
+}
+
+WModule::SPtr WMFiffReader::factory() const
+{
+    return WModule::SPtr( new WMFiffReader );
+}
+
+const char** WMFiffReader::getXPMIcon() const
+{
+    return module_xpm;
+}
+
+void WMFiffReader::connectors()
+{
+    WModule::connectors();
+    m_output.reset(
+                    new WLModuleOutputDataCollectionable< WLEMMCommand >( shared_from_this(), "out",
+                                    "Provides a filtered EMM-DataSet" ) );
+    addConnector( m_output );
+}
+
+void WMFiffReader::properties()
+{
+    WModule::properties();
+
+    m_propCondition = WCondition::SPtr( new WCondition() );
+
+    m_trgSendEMM = m_properties->addProperty( "Send EMM:", "Send", WPVBaseTypes::PV_TRIGGER_READY, m_propCondition );
+
+    // FIFF file //
+    m_propFiffFile = m_properties->addProperty( "FIFF file:", "FIFF file to load.", WPathHelper::getHomePath(), m_propCondition );
+    m_propFiffFile->changed( true );
+
+    m_propFileStatus = m_properties->addProperty( "File status:", "FIFF file status.",
+                    EFileStatus::name( EFileStatus::NO_FILE ) );
+    m_propFileStatus->setPurpose( PV_PURPOSE_INFORMATION );
+
+    // Additional data //
+    m_propDataStatus = m_properties->addProperty( "Data status:", "Status of additional data.",
+                    EDataStatus::name( EDataStatus::NO_DATA ) );
+    m_propDataStatus->setPurpose( PV_PURPOSE_INFORMATION );
+
+    m_trgLoadData = m_properties->addProperty( "Load data", "Load data", WPVBaseTypes::PV_TRIGGER_READY, m_propCondition );
+    m_trgLoadData->setHidden( true );
+
+    const std::string subject( "none" );
+    m_propSubject = m_properties->addProperty( "Subject:", "Selected a subject.", subject );
+    m_propSubject->setHidden( true );
+    m_propSubject->setPurpose( PV_PURPOSE_INFORMATION );
+
+    const std::string trial( "none" );
+    m_propTrial = m_properties->addProperty( "Trial:", "Selected a trial.", trial );
+    m_propTrial->setHidden( true );
+    m_propTrial->setPurpose( PV_PURPOSE_INFORMATION );
+
+    m_itmBemFiles = WItemSelection::SPtr( new WItemSelection() );
+
+    m_itmSurfaces = WItemSelection::SPtr( new WItemSelection() );
+
+}
+
+void WMFiffReader::moduleInit()
+{
+    infoLog() << "Initializing module ...";
+    // init moduleState for using Events in mainLoop
+    m_moduleState.setResetable( true, true ); // resetable, autoreset
+    m_moduleState.add( m_propCondition ); // when properties changed
+
+    ready(); // signal ready state
+    waitRestored();
+}
+
+void WMFiffReader::moduleMain()
+{
+    moduleInit();
+
+    while( !m_shutdownFlag() )
+    {
+        m_moduleState.wait();
+        if( m_shutdownFlag() )
+        {
+            break;
+        }
+
+        if( m_propFiffFile->changed( true ) )
+        {
+            handleFiffFileChanged();
+        }
+
+        if( m_trgLoadData->get( true ) == WPVBaseTypes::PV_TRIGGER_TRIGGERED )
+        {
+            handleTrgLoad();
+        }
+
+        if( m_trgSendEMM->get( true ) == WPVBaseTypes::PV_TRIGGER_TRIGGERED )
+        {
+            handleTrgSendEMM();
+        }
+    }
+}
+
+void WMFiffReader::handleTrgSendEMM()
+{
+    debugLog() << "handleTrgSendEMM() called!";
+    if( !m_emm )
+    {
+        errorLog() << "No EMM object to send!";
+        return;
+    }
+    if( m_subject.get() )
+    {
+        m_emm->setSubject( m_subject );
+    }
+
+    WLEMMCommand::SPtr cmd = WLEMMCommand::instance( WLEMMCommand::Command::COMPUTE );
+    cmd->setEmm( m_emm );
+    m_output->updateData( cmd );
+    infoLog() << "Sent EMM object.";
+    m_trgSendEMM->set( WPVBaseTypes::PV_TRIGGER_READY, true );
+}
+
+void WMFiffReader::handleFiffFileChanged()
+{
+    updateFileStatus( EFileStatus::LOADING_FILE );
+    const std::string fName = m_propFiffFile->get().string();
+    if( readFiffFile( fName ) )
+    {
+        updateFileStatus( EFileStatus::SUCCESS );
+        updateDataStatus( EDataStatus::LOADING_DATA );
+        if( retrieveAdditionalData( fName ) )
+        {
+            updateDataStatus( EDataStatus::DATA_AVAILABLE );
+        }
+        else
+        {
+            updateDataStatus( EDataStatus::NO_DATA );
+        }
+    }
+    else
+    {
+        updateFileStatus( EFileStatus::FILE_ERROR );
+    }
+}
+
+bool WMFiffReader::readFiffFile( const std::string& fName )
+{
+    infoLog() << "Reading FIFF file: " << fName;
+    WLReaderFIFF::SPtr fiffReader;
+    try
+    {
+        fiffReader.reset( new WLReaderFIFF( fName ) );
+        m_emm.reset( new WLEMMeasurement() );
+        if( fiffReader->Read( m_emm ) == WLReaderFIFF::ReturnCode::SUCCESS )
+        {
+            if( m_emm->hasModality( WLEModality::EEG ) )
+            {
+                WLEMDEEG::SPtr eeg = m_emm->getModality< WLEMDEEG >( WLEModality::EEG );
+                if( eeg->getFaces()->empty() )
+                {
+                    warnLog() << "No faces found! Faces will be generated.";
+                    WLGeometry::computeTriangulation( eeg->getFaces().get(), *eeg->getChannelPositions3d(), -5 );
+                }
+            }
+            infoLog() << "Modalities:\t" << m_emm->getModalityCount();
+            infoLog() << "Event channels:\t" << m_emm->getEventChannelCount();
+            infoLog() << "Reading FIFF file finished!";
+            return true;
+        }
+    }
+    catch( const WException& e )
+    {
+        errorLog() << "Error reading FIFF file: " << e.what();
+    }
+    return false;
+}
+
+bool WMFiffReader::retrieveAdditionalData( const std::string& fName )
+{
+    bool hasData = false;
+    boost::filesystem::path fiffFile( fName );
+    boost::filesystem::path expRoot = WLReaderExperiment::getExperimentRootFromFiff( fiffFile );
+    const std::string subject = WLReaderExperiment::getSubjectFromFiff( fiffFile );
+    const std::string trial = WLReaderExperiment::getTrialFromFiff( fiffFile );
+
+    m_propSubject->set( subject );
+    m_propSubject->setHidden( false );
+    m_propTrial->set( trial );
+    m_propTrial->setHidden( false );
+
+    if( !boost::filesystem::is_directory( expRoot ) )
+    {
+        expRoot = expRoot.parent_path();
+    }
+    m_expReader.reset( new WLReaderExperiment( expRoot.string(), subject ) );
+
+    std::set< std::string > bems = m_expReader->findBems();
+    m_itmBemFiles->clear();
+    m_properties->removeProperty( m_selBemFiles );
+    if( !bems.empty() )
+    {
+        hasData = true;
+        std::set< std::string >::iterator itBEMs;
+        for( itBEMs = bems.begin(); itBEMs != bems.end(); ++itBEMs )
+        {
+            m_itmBemFiles->addItem(
+                            WItemSelectionItemTyped< string >::SPtr(
+                                            new WItemSelectionItemTyped< string >( *itBEMs, *itBEMs ) ) );
+        }
+        m_selBemFiles = m_properties->addProperty( "BEM Layers:", "Select BEM Layers to use.",
+                        m_itmBemFiles->getSelectorFirst() );
+
+        // Be sure it is at least one selected, but not more than one
+        WPropertyHelper::PC_SELECTONLYONE::addTo( m_selBemFiles );
+        m_trgLoadData->setHidden( false );
+    }
+
+    m_itmSurfaces->clear();
+    m_properties->removeProperty( m_selSurfaces );
+    std::set< std::string > surfaces = m_expReader->findSurfaceKinds();
+    if( !surfaces.empty() )
+    {
+        hasData = true;
+        std::set< std::string >::iterator itSurfs;
+        for( itSurfs = surfaces.begin(); itSurfs != surfaces.end(); ++itSurfs )
+        {
+            m_itmSurfaces->addItem(
+                            WItemSelectionItemTyped< string >::SPtr(
+                                            new WItemSelectionItemTyped< string >( *itSurfs, *itSurfs ) ) );
+        }
+        m_selSurfaces = m_properties->addProperty( "Surfaces:", "Select surface to use.", m_itmSurfaces->getSelectorFirst() );
+
+        // Be sure it is at least one selected, but not more than one
+        WPropertyHelper::PC_SELECTONLYONE::addTo( m_selSurfaces );
+        m_trgLoadData->setHidden( false );
+    }
+
+    return hasData;
+}
+
+void WMFiffReader::handleTrgLoad()
+{
+    infoLog() << "Reading additional data ...";
+    updateDataStatus( EDataStatus::LOADING_DATA );
+    if( readData() )
+    {
+        updateDataStatus( EDataStatus::SUCCESS );
+    }
+    else
+    {
+        updateDataStatus( EDataStatus::DATA_ERROR );
+    }
+    infoLog() << "Finish reading additional data!";
+    m_trgLoadData->set( WPVBaseTypes::PV_TRIGGER_READY, true );
+}
+
+bool WMFiffReader::readData()
+{
+    bool succes = false;
+    m_subject.reset( new WLEMMSubject() );
+
+    const string bemFile = m_selBemFiles->get().at( 0 )->getAs< WItemSelectionItemTyped< string > >()->getValue();
+    succes |= m_expReader->readBem( bemFile, m_subject );
+
+    const string surfaceType = m_selSurfaces->get().at( 0 )->getAs< WItemSelectionItemTyped< string > >()->getValue();
+    succes |= m_expReader->readSourceSpace( surfaceType, m_subject );
+
+    const string trial = m_propTrial->get();
+    succes |= m_expReader->readLeadFields( surfaceType, bemFile, trial, m_subject );
+
+    return succes;
+}
---
+++ src/modules/fiffReader/WMFiffReader.h
@@ -0,0 +1,143 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMFIFFREADER_H_
+#define WMFIFFREADER_H_
+
+#include <string>
+
+#include <core/common/WCondition.h>
+#include <core/common/WItemSelection.h>
+#include <core/common/WPropertyTypes.h>
+#include <core/kernel/WModule.h>
+
+#include "core/data/WLEMMCommand.h"
+#include "core/data/WLEMMeasurement.h"
+#include "core/data/WLEMMSubject.h"
+#include "core/io/WLReaderExperiment.h"
+#include "core/module/WLModuleOutputDataCollectionable.h"
+
+/**
+ * Reads a FIFF file and retrieves additional data, e.g. BEMs, surfaces or leadfields.
+ *
+ * \author pieloth
+ */
+class WMFiffReader: public WModule
+{
+public:
+    WMFiffReader();
+    virtual ~WMFiffReader();
+
+    virtual const std::string getName() const;
+
+    virtual const std::string getDescription() const;
+
+    virtual WModule::SPtr factory() const;
+
+    virtual const char** getXPMIcon() const;
+
+protected:
+    virtual void connectors();
+
+    virtual void properties();
+
+    virtual void moduleInit();
+
+    virtual void moduleMain();
+
+private:
+    LaBP::WLModuleOutputDataCollectionable< WLEMMCommand >::SPtr m_output;
+
+    /**
+     * A condition used to notify about changes in several properties.
+     */
+    WCondition::SPtr m_propCondition;
+
+    WLEMMeasurement::SPtr m_emm;
+    WLEMMSubject::SPtr m_subject;
+
+    WPropTrigger m_trgSendEMM;
+    void handleTrgSendEMM();
+
+    struct EFileStatus
+    {
+        enum Enum
+        {
+            NO_FILE, LOADING_FILE, FILE_ERROR, SUCCESS
+        };
+
+        static std::string name( Enum val );
+    };
+    EFileStatus::Enum m_fileStatus;
+    WPropString m_propFileStatus;
+    void updateFileStatus( EFileStatus::Enum status );
+
+    WPropFilename m_propFiffFile;
+    void handleFiffFileChanged();
+    bool readFiffFile( const std::string& fName );
+
+    struct EDataStatus
+    {
+        enum Enum
+        {
+            NO_DATA, DATA_AVAILABLE, LOADING_DATA, DATA_ERROR, SUCCESS
+        };
+
+        static std::string name( Enum val );
+    };
+    EDataStatus::Enum m_dataStatus;
+    WPropString m_propDataStatus;
+    void updateDataStatus( EDataStatus::Enum status );
+
+    WPropTrigger m_trgLoadData;
+    void handleTrgLoad();
+    bool readData();
+
+    bool retrieveAdditionalData( const std::string& fName );
+    WLReaderExperiment::SPtr m_expReader;
+
+    WPropString m_propSubject;
+
+    WItemSelection::SPtr m_itmBemFiles;
+    WPropSelection m_selBemFiles;
+
+    WItemSelection::SPtr m_itmSurfaces;
+    WPropSelection m_selSurfaces;
+
+    WPropString m_propTrial;
+};
+
+inline void WMFiffReader::updateFileStatus( EFileStatus::Enum status )
+{
+    m_fileStatus = status;
+    m_propFileStatus->set( EFileStatus::name( status ), true );
+}
+
+inline void WMFiffReader::updateDataStatus( EDataStatus::Enum status )
+{
+    m_dataStatus = status;
+    m_propDataStatus->set( EDataStatus::name( status ), true );
+}
+
+#endif  // WMFIFFREADER_H_
