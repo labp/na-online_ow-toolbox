d7a2c89470571b79d40c938e7a26dd1f3fa87b16
Christof Pieloth <pieloth@labp.htwk-leipzig.de>
2014-10-16 16:24:08 +0200

    [STYLE] Add license header

--- src/modules/firFilter/test/WFIRFilter_test.h
+++ src/modules/firFilter/test/WFIRFilter_test.h
@@ -1,24 +1,23 @@
 //---------------------------------------------------------------------------
 //
-// Project: OpenWalnut ( http://www.openwalnut.org )
+// Project: NA-Online ( http://www.labp.htwk-leipzig.de )
 //
-// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
-// For more information see http://www.openwalnut.org/copying
+// Copyright 2010 Laboratory for Biosignal Processing, HTWK Leipzig, Germany
 //
-// This file is part of OpenWalnut.
+// This file is part of NA-Online.
 //
-// OpenWalnut is free software: you can redistribute it and/or modify
+// NA-Online is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// OpenWalnut is distributed in the hope that it will be useful,
+// NA-Online is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+// along with NA-Online. If not, see <http://www.gnu.org/licenses/>.
 //
 //---------------------------------------------------------------------------
 
@@ -52,7 +51,6 @@
 class WFIRFilterTest: public CxxTest::TestSuite
 {
 public:
-
     void setUp( void )
     {
         WLogger::startup();
@@ -118,7 +116,7 @@ public:
         std::vector< WFIRFilter::ScalarT > coeffs;
         for( size_t i = 0; i < ORDER + 1; ++i )
         {
-            // TODO (pieloth) set correct coefficients
+            // TODO(pieloth): set correct coefficients
             coeffs.push_back( i );
         }
 
@@ -134,7 +132,7 @@ public:
         std::vector< WFIRFilter::ScalarT > coeffs;
         for( size_t i = 0; i < ORDER + 1; ++i )
         {
-            // TODO (pieloth) set correct coefficients
+            // TODO(pieloth): set correct coefficients
             coeffs.push_back( i );
         }
 
@@ -150,7 +148,7 @@ public:
         std::vector< WFIRFilter::ScalarT > coeffs;
         for( size_t i = 0; i < ORDER + 1; ++i )
         {
-            // TODO (pieloth) set correct coefficients
+            // TODO(pieloth): set correct coefficients
             coeffs.push_back( i );
         }
 
@@ -166,7 +164,7 @@ public:
         std::vector< WFIRFilter::ScalarT > coeffs;
         for( size_t i = 0; i < ORDER + 1; ++i )
         {
-            // TODO (pieloth) set correct coefficients
+            // TODO(pieloth): set correct coefficients
             coeffs.push_back( i );
         }
 
@@ -299,4 +297,4 @@ public:
     }
 };
 
-#endif // WFIRFILTER_TEST_H
+#endif  // WFIRFILTER_TEST_H
